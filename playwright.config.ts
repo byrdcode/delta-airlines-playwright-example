5619a5583bc470e8a1072c33a19039ca7484ff7a
byrdcode <carlbyrd@gmail.com>
2023-04-17 07:55:44 -0700

    headless true

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.delta.com/',
     testIdAttribute: 'id',
-    headless: false,
+    // headless: false,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
